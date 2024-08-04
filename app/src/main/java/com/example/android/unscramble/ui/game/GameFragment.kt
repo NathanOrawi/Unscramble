562bbacded641133ec68340a871e7bfdb67dc968
NathanOrawi <nathan.orawi@my.jcu.edu.au>
2024-08-04 22:52:06 +1000

    Attach observer to the score and word count

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -62,15 +62,21 @@ class GameFragment : Fragment() {
 //        binding.shuffle.setOnClickListener { onShuffleWord() }
 //        binding.hint.setOnClickListener { onHintWord() }
         // Update the UI
-        binding.score.text = getString(R.string.score, 0)
-        binding.incorrectWordCount.text = getString(
-                R.string.incorrect_word_count, 0, MAX_NO_OF_INCORRECT_WORDS)
-        binding.wordCount.text = getString(
-                R.string.word_count, 0, MAX_NO_OF_WORDS)
+        //binding.incorrectWordCount.text = getString( R.string.incorrect_word_count, 0, MAX_NO_OF_INCORRECT_WORDS)
         // Observe the currentScrambledWord LiveData
         viewModel.currentScrambledWord.observe(viewLifecycleOwner)
         { newWord ->
             binding.textViewScrambledWord.text = newWord
+        }
+        // Observe the score LiveData
+        viewModel.score.observe(viewLifecycleOwner)
+        { newScore ->
+            binding.score.text = getString(R.string.score, newScore)
+        }
+        // Observe the currentWordCount LiveData
+        viewModel.currentWordCount.observe(viewLifecycleOwner)
+        { newWordCount ->
+            binding.wordCount.text = getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
         }
     }
     /*
